191d32aa8f327d8de31d5a5e3ccd5f2095d929d2
Fals Kompas <falskompas1@gmail.com>
2019-07-22 16:16:17 +0700

    web asli

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/perpustakaan_joss';
+$config['base_url'] = 'http://localhost/perpustakaansekolah';
 
 /*
 |--------------------------------------------------------------------------
